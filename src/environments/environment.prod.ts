a85cd298553525c3a50b22155d98f239f821c073
Gabriele Napoli <gabriele.nap@gmail.com>
2023-07-13 13:05:34 +0200

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,23 +1,4 @@
-import * as config from '../../auth_config.json';
-
-const { domain, clientId, audience, apiUri, errorPath } = config as {
-  domain: string;
-  clientId: string;
-  audience?: string;
-  apiUri: string;
-  errorPath: string;
-};
-
 export const environment = {
-  production: true,
-  auth: {
-    domain,
-    clientId,
-    ...(audience && audience !== "YOUR_API_IDENTIFIER" ? { audience } : null),
-    redirectUri: window.location.origin,
-    errorPath,
-  },
-  httpInterceptor: {
-    allowedList: [`${apiUri}/*`],
-  },
+  production: false,
+  apiUrl: 'https://gifted-shirt-pike.cyclic.app',
 };
